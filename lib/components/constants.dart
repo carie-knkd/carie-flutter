139c329b10de58aa31a6a42b9de3a7b6d3b77901
huyvu2001 <41413347+huyvu2001@users.noreply.github.com>
2021-04-11 20:09:56 +0700

    update popup

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xFF8082E5);
+const Color kPrimaryColor = Color(0xFF4FC6E2);
 const Color kTextColor = Color(0xFF3C4046);
 const Color kBackgroundColor = Color(0xFFF9F8FD);
 
--- lib/components/driver_info_widget.dart
+++ lib/components/driver_info_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_go_app/components/constants.dart';
 import 'package:flutter_go_app/model/Driver.dart';
 
 class DriverInfoWidget extends StatelessWidget {
@@ -35,16 +36,29 @@ class DriverInfoWidget extends StatelessWidget {
 }
 
 void showDriverInfo(BuildContext context, Driver driver) {
+  double height = MediaQuery.of(context).size.height;
   showDialog(
       context: context,
-      builder: (BuildContext context) => AlertDialog(
-            title: Text("Book this driver?"),
-            content: Text(driver.toString()),
-            actions: [
-              Center(
-                child: ElevatedButton(
-                    onPressed: () => print("BOOKED"), child: Text("Book")),
+      builder: (BuildContext context) => Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0)),
+            child: Container(
+              height: height / 2,
+              width: height / 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'lib/images/empty-avatar.jpg',
+                    scale: 750 / height,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print("BOOK"),
+                    child: Text("BOOK TÀI XẾ"),
+                    style: ElevatedButton.styleFrom(primary: kPrimaryColor),
+                  ),
+                ],
               ),
-            ],
+            ),
           ));
 }
